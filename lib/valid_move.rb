93e4dee5a25c6520d64ce0db57e7e378b329de14
Cuong Phan <at120906@actvn.edu.vn>
2020-01-05 03:24:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,18 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
+<<<<<<< HEAD
   if index < 9 && index > -1 && !position_taken?(board, index)
     true
   else
     false
+=======
+  if index > 9 || index < 1
+    if position_taken?(board, index)
+      false || nil
+    end
+  else
+    true
+>>>>>>> 759a1888c120aac37f93c087b37bded388bd3749
   end
 end
 
